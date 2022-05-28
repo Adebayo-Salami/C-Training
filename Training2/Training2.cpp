65ca3c63a03ee4258fa1d1e512851e07c34579dc
Adebayo-Salami <salamibolarinwa16@gmail.com>
2022-05-28 13:42:41 +0100

    Pop

--- Training2/Training2.cpp
+++ Training2/Training2.cpp
@@ -32,6 +32,9 @@ void NonRecursiveFactorialQ(unsigned long int n);
 void RationalCode();
 void TemplateFunctions();
 void StandardFileIO();
+void BinaryFileIO();
+void UnFormattedIO();
+void FormattedIO();
 
 int main()
 { 
@@ -117,11 +120,20 @@ int main()
     printf("\n");
     RationalCode();*/
 
-    printf("\n");
-    TemplateFunctions();
+    //printf("\n");
+    //TemplateFunctions();
+
+    //printf("\n");
+    //StandardFileIO();
+
+    //printf("\n");
+    //void BinaryFileIO();
 
     printf("\n");
-    StandardFileIO();
+    UnFormattedIO();
+
+    printf("\n");
+    FormattedIO();
 
     return 0;
 }
@@ -719,4 +731,72 @@ void StandardFileIO() {
     fclose(fr);
     remove(fn);
     puts("done");
+}
+
+constexpr size_t maxlen = 127;
+struct s1 {
+    uint8_t num;
+    uint8_t len;
+    char s[maxlen + 1];
+};
+void BinaryFileIO() {
+    const char* fn = "test.file";            // file name
+    const char* str = "This is a literal C-string.";
+
+    // create/write the file
+    puts("writing file");
+    FILE* fw = fopen(fn, "wb");
+
+    static s1 buf1;
+    for (int i = 0; i < 5; i++) {
+        buf1.num = i;
+        buf1.len = (uint8_t)strlen(str);
+        if (buf1.len > maxlen) buf1.len = maxlen;
+        strncpy(buf1.s, str, maxlen);
+        buf1.s[buf1.len] = 0; // make sure it's terminated
+        fwrite(&buf1, sizeof(s1), 1, fw);
+    }
+
+    fclose(fw);
+    puts("done.");
+
+    // read the file
+    puts("reading file");
+    FILE* fr = fopen(fn, "rb");
+    static s1 buf2;
+    size_t rc;
+    while ((rc = fread(&buf2, sizeof(s1), 1, fr))) {
+        printf("a: %d, b: %d, s: %s\n", buf2.num, buf2.len, buf2.s);
+    }
+
+    fclose(fr);
+    remove(fn);
+
+    puts("done.");
+}
+
+void UnFormattedIO() {
+    const int bufsize = 256;
+    static char buf[bufsize];
+    fputs("prompt: ", stdout);
+    fgets(buf, bufsize, stdin);
+    puts("output:");
+    fputs(buf, stdout);
+    fflush(stdout);
+}
+
+void FormattedIO() {
+    int i = 5;
+    long int li = 1234567890L;
+    const char * s = "This is a c string.";
+    //%d -> Integer
+    //%f -> Float
+    //%s -> C-string
+    //%c -> Char
+    //%p -> Pointer
+    //%% -> The % Character
+    printf("i is %d, li is %ld, s is %s \n", i, li, s);
+    printf("i is %4d, li is %ld, s is %s \n", i, li, s);
+    printf("i is %-4d, li is %ld, s is %s \n", i, li, s);
+    printf("i is %04d, li is %ld, s is %s \n", i, li, s);
 }
