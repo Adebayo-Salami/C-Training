81ebc3c8f32f604fd978454ada5ce4db00c87258
Adebayo-Salami <salamibolarinwa16@gmail.com>
2022-09-13 22:48:43 +0100

    Push

---
+++ Training3/Training3.cpp
@@ -0,0 +1,50 @@
+// Training3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <typeinfo>
+
+void Templates();
+
+int main()
+{
+    printf("\n");
+    Templates();
+}
+
+template<typename T>
+constexpr T pi = T(3.1415926535897932385L);
+
+template<typename T>
+T area_of_circle(const T& r) {
+    return r * r * pi<T>;
+ }
+
+template <typename lT, typename rT>
+auto tf(const lT& lhs, const rT& rhs) {
+    return lhs + rhs;
+}
+
+void Templates() {
+    std::cout.precision(20);
+    std::cout << pi<long double> << " " << area_of_circle<long double>(3) << std::endl;
+
+    const char * cstr = "this is a c-string";
+    const std::string sclass = std::string("this is a string class string");
+
+    auto x = "this is a c-string";
+    decltype(sclass) y = "Fatai";
+
+    for (auto it = sclass.begin(); it != sclass.end(); ++it) {
+        std::cout << *it << " ";
+    }
+
+    for (auto c : sclass) {
+        std::cout << c << " ";
+    }
+
+    auto z = tf<std::string, const char*>(sclass, cstr);
+    std::cout << "z is " << z << std::endl; 
+    std::cout << "type of z is " << typeid(z).name() << std::endl;
+}
