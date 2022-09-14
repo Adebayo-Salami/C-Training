6dbd010cbbfc37251369225cd2a882899c17c40f
Adebayo-Salami <salamibolarinwa16@gmail.com>
2022-09-14 22:55:12 +0100

    as

--- Training3/Training3.cpp
+++ Training3/Training3.cpp
@@ -10,6 +10,7 @@
 #include <array>
 #include <deque>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -21,6 +22,7 @@ void STLContainers_Tuple();
 void STLContainers_Array();
 void STLContainers_Deque();
 void STLContainers_Queue();
+void STLContainers_Stack();
 
 int main()
 {
@@ -47,6 +49,9 @@ int main()
 
     printf("\n");
     STLContainers_Queue();
+
+    printf("\n");
+    STLContainers_Stack();
 }
 
 template<typename T>
@@ -380,5 +385,32 @@ void STLContainers_Queue() {
     }
     cout << endl;
     report_queue(q2);
+}
+
+void STLContainers_Stack() {
+    // vector
+    message("stk1");
+    vector<int> v1 = { 1, 2, 3, 4, 5 };
+    stack<int, vector<int>> stk1(v1);
+    report_stack(stk1);
+    message("stk1 push 42");
+    stk1.push(42);
+    report_stack(stk1);
+    message("stk1 pop");
+    stk1.pop();
+    report_stack(stk1);
+
+    message("stk2 default stack (deque)");
+    stack<string> stk2;
+    for (string s : {"one", "two", "three", "four", "five"}) {
+        stk2.push(s);
+    }
+    report_stack(stk2);
+    message("stk2 push forty-two");
+    stk2.push("forty-two");
+    report_stack(stk2);
+    message("stk2 pop");
+    stk2.pop();
+    report_stack(stk2);
 }
 
