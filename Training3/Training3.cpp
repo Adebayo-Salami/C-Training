b381c99d249b267d5eeff1441e5f1dd88e7b4aaa
Adebayo-Salami <salamibolarinwa16@gmail.com>
2022-09-14 23:09:09 +0100

    as

--- Training3/Training3.cpp
+++ Training3/Training3.cpp
@@ -13,6 +13,7 @@
 #include <stack>
 #include <set>
 #include <unordered_set>
+#include <map>
 
 using namespace std;
 
@@ -26,6 +27,7 @@ void STLContainers_Deque();
 void STLContainers_Queue();
 void STLContainers_Stack();
 void STLContainers_Sets();
+void STLContainers_Maps();
 
 int main()
 {
@@ -57,7 +59,7 @@ int main()
     STLContainers_Stack();
 
     printf("\n");
-    STLContainers_Sets();
+    STLContainers_Maps();
 }
 
 template<typename T>
@@ -144,6 +146,20 @@ template<typename T>
 void print_set(T& s) {
     if (s.empty()) return;
     for (auto x : s) cout << x << " ";
+    cout << endl;
+}
+
+// print a pair
+template <typename T1, typename T2>
+void print_pair(pair<T1, T2>& p) {
+    cout << p.first << ": " << p.second << endl;
+}
+
+// print the elements of the map
+template<typename T>
+void print_map(T& m) {
+    if (m.empty()) return;
+    for (auto x : m) print_pair(x);
     cout << endl;
 }
 
@@ -436,6 +452,7 @@ void STLContainers_Stack() {
 void STLContainers_Sets() {
     message("construct set set1");
     set<string> set1 = { "one", "two", "three", "four", "five" };
+    multiset<string> multiset1 = { "one", "two", "three", "four", "five" }; //multiset allows duplicate
     message("size of set", set1.size());
     message("ordered set is alphabetical");
     print_set(set1);
@@ -463,7 +480,8 @@ void STLContainers_Sets() {
 
 
     message("construct unordered set set1");
-    unordered_set<string> set2 = { "one", "two", "three", "four", "five" };
+    unordered_set<string> set2 = { "one", "two", "three", "four", "five" }; // Seaching is faster returns hash to represent value
+    unordered_multiset<string> multisetset2 = { "one", "two", "three", "four", "five" };    // Allows duplicate
     message("size of set", set2.size());
     message("unordered set has no defined order");
     print_set(set2);
@@ -487,5 +505,50 @@ void STLContainers_Sets() {
     set2.insert("five");
     print_set(set2);
     cout << endl;
+}
+
+void STLContainers_Maps() {
+    message("map of strings from initializer list");
+    map<string, string> mapstr = { { "George", "Father" }, { "Ellen", "Mother" },
+        { "Ruth", "Daughter" }, { "Spike", "Neighbor's Son" } };
+
+    message("size", mapstr.size());
+    message("get some values");
+    message("George", mapstr["George"]);
+    message("Ellen", mapstr.at("Ellen"));
+    message("Spike", mapstr.find("Spike")->second);
+    message("print_map:");
+    print_map(mapstr);
+
+    message("insert an element");
+    mapstr.insert({ "Luke", "Neighbor" });
+    message("size", mapstr.size());
+    print_map(mapstr);
+
+    cout << "insert a duplicate:" << endl;
+    auto rp = mapstr.insert({ "Luke", "Neighbor" });
+    if (rp.second) {
+        message("insert succeeded");
+        print_pair(*rp.first);
+    }
+    else {
+        message("insert failed");
+    }
+    message("size", mapstr.size());
+    print_map(mapstr);
+
+    message("find and erase Spike");
+    auto it = mapstr.find("Spike");
+    if (it != mapstr.end()) {
+        message("found");
+        print_pair(*it);
+        mapstr.erase(it);
+        message("erased");
+    }
+    else {
+        message("not found");
+    }
+    message("size", mapstr.size());
+    print_map(mapstr);
 }
 
