3cf996b77913f3b024e4827dbe80cf6716f0055d
Adebayo-Salami <salamibolarinwa16@gmail.com>
2022-09-17 17:15:54 +0100

    asa

--- Training3/Training3.cpp
+++ Training3/Training3.cpp
@@ -968,9 +968,30 @@ void STLFunctions_Relational() {
     not_equal_to<long> f;
     transform(v1.begin(), v1.end(), v2.begin(), v3.begin(), f);
     disp_v(v3);
+
+    //greater<long> ff;
+    less<long> ff;
+    sort(v2.begin(), v2.end(), ff);
+
+    disp_v(v3);
 }
 
+template <typename T>
+void disp_v(T& v) {
+    if (!v.size()) return;
+    for (auto e : v) { cout << e << " "; }
+    cout << endl;
+}
 void STLFunctions_Logical() {
+    vector<int> v1 = { 1, 0, 1, 0, 1, 0, 1, 0 };
+    vector<int> v2 = { 1, 1, 1, 1, 0, 0, 0, 0 };
+    vector<int> v3(v1.size(), 0);
+    disp_v(v1);
+    disp_v(v2);
+    cout << endl;
 
+    logical_and<int> f;
+    transform(v1.begin(), v1.end(), v2.begin(), v3.begin(), f);
+    disp_v(v3);
 }
 
